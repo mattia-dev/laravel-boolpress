589abcc083e2d3548f7cc28027797c8814090175
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-09-14 19:07:37 +0200

    improved style

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,27 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="post-container container">
+        <img class="banner" src="{{ $post->image }}" alt="banner">
+
         @if( $post->premium_content )
             <div class="premium">PREMIUM</div>
         @endif
 
-        <h1>{{ $post->title }}</h1>
+        <h1>{{ substr($post->title, 0, -1) }}</h1>
 
-        <div class="author"><i class="bi bi-pen-fill"></i>{{ $post->author }}</div>
+        <div class="author"><i class="bi bi-pen-fill"></i>Written by {{ ucwords($post->author) }}</div>
 
-        <div class="post-details">
-            <div class="publish-date"><i class="bi bi-calendar-check-fill"></i> {{ $post->publish_date }}</div>
+        <div class="post-details d-flex justify-content-between">
+            <div class="publish-date"><i class="bi bi-calendar-check-fill"></i>Published on {{ date('d F Y', strtotime($post->publish_date)) }} at {{ date('H:i', strtotime($post->publish_date)) }}</div>
 
-            <div class="tags"><i class="bi bi-tags-fill"></i>{{ $post->tags }} {{$post->premium_content ? "premium" : "free"}}</div>
+            <div class="tags"><i class="bi bi-tags-fill"></i>{{ ucwords(implode(', ', explode(' ', $post->tags))) }}{{$post->premium_content ? ", Premium" : "free"}}</div>
 
             <div class="views"><i class="bi bi-eye-fill"></i>Read {{ $post->views }} times</div>
         </div>
 
-        <div class="clearfix">
+        <div class="post-body clearfix">
             <img class="float-right" src="{{ $post->image }}" alt="post image">
 
             <p>{{ $post->body }}</p>
+        </div>
+
+        <div class="comment-section">
+            <h3>COMMENTS</h3>
+
+            <form>
+                <div class="input-group mb-3">
+                    <span class="input-group-text">Username</span>
+                    <input type="text" class="form-control">
+                </div>
+
+                <textarea class="form-control mb-3" rows="5"></textarea>
+
+                <button type="submit" class="btn btn-primary">Comment</button>
+            </form>
         </div>
     </div>
 @endsection
