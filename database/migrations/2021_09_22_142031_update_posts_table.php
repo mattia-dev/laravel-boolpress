83da22332c875d40d2ef820e2df9f9f5b18575f8
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-09-22 16:23:14 +0200

    create database one to many relation between users and posts

---
+++ database/migrations/2021_09_22_142031_update_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('publish_date');
+            $table->dropColumn('author');
+            $table->unsignedBigInteger('user_id')->after('title');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_user_id_foreign');
+            $table->dropColumn('user_id');
+            $table->string('author');
+            $table->dateTime('publish_date');
+        });
+    }
+}
